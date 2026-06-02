ac10cd6c0dcb7a9f2ce5fc3c900f96a3f53f5f41
İsmail Ilgar <ismailthedrizzt@gmail.com>
2026-06-02 22:36:38 +0300

    Allow app1 live origin

--- next.config.js
+++ next.config.js
@@ -69,6 +69,7 @@ const nextConfig = {
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     'http://0.0.0.0:3000',
+    'https://app1.edengrup.com',
   ],
   experimental: {
     // App Router is stable in Next.js 14
